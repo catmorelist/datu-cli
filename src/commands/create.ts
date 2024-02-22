10951663624500319cd9425976e60d8e05145700
LAPTOP-HDSRAK7I\gcp <852057623@qq.com>
2024-02-22 17:46:42 +0800

    检查版本更新

--- src/commands/create.ts
+++ src/commands/create.ts
@@ -1,7 +1,11 @@
-import { input, select } from '@inquirer/prompts';
-import {clone} from '../utils/clone';
 import path from 'path';
 import fs from 'fs-extra';
+import { gt } from 'lodash';
+import chalk from 'chalk';
+import axios, { AxiosResponse } from 'axios';
+import { input, select } from '@inquirer/prompts';
+import { clone } from '../utils/clone';
+import { name, version } from '../../package.json';
 
 export interface TemplateInfo {
   name: string; // 模板名称
@@ -30,6 +34,7 @@ export const templates: Map<string, TemplateInfo> = new Map([
   ],
 ]);
 
+
 const isOverWrite = (projectName: string) => {
   return select({
     message: '文件夹已存在，是否覆盖？',
@@ -39,6 +44,37 @@ const isOverWrite = (projectName: string) => {
     ]
   })
 }
+
+export const getNpmInfo = async (npmName: string) => {
+  const npmUrl = `https://registry.npmjs.org/${name}`;
+  let res = {};
+  try {
+      res = await axios.get(npmUrl);
+  } catch (error) {
+      console.error(error);
+  }
+  return res;
+};
+
+export const getNpmLatestVersion = async (name: string) => {
+  const { data } = (await getNpmInfo(name)) as AxiosResponse;
+  return data['dist-tags'].latest;
+};
+
+
+export const checkVersion = async (name: string, version: string) => {
+  const latestVersion = await getNpmLatestVersion(name);
+  const need = gt(latestVersion, version);
+  if (need) {
+      console.warn(
+          `检查到dawei最新版本： ${chalk.blackBright(latestVersion)}，当前版本是：${chalk.blackBright(version)}`
+      );
+      console.log(
+          `可使用： ${chalk.yellow('npm install dawei-cli@latest')}，或者使用：${chalk.yellow('dawei update')}更新`
+      );
+  }
+  return need;
+};
 
 export async function create(projectName: string) {
   const templateList = Array.from(templates).map((item:[string,TemplateInfo]) => {
@@ -66,6 +102,9 @@ export async function create(projectName: string) {
       return
     }
   }
+
+  // 检查版本更新
+  await checkVersion(name, version);
 
   const templateName = await select({
     message: '请选择模板',
